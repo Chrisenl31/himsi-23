51ef1fd4f4c856abc5ec221991e92339fb1bdeb7
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2023-07-22 06:28:26 +0700

    taroh detail di client

--- resources/views/alumni/client/index.blade.php
+++ resources/views/alumni/client/index.blade.php
@@ -43,7 +43,10 @@
                             <td><a href="">linkedin</a></td>
                             <td>darfito@gmail.com</td>
                             <td>Software Developer</td>
-                            <td><a href=""><img src="{{ url('assets/img/mercusuar/icon/info.svg') }}"></a></td>
+                            <td>
+                                <button class="show-info"><a href=""><img
+                                            src="{{ url('assets/img/mercusuar/icon/info.svg') }}"></a></button>
+                            </td>
                         </tr>
                         <tr>
                             <td>Cakra Kusuma Erlangga Ramdani</td>
@@ -62,10 +65,78 @@
                 <button>2</button>
                 <button>&gt</button>
             </div>
+
+            {{-- modal --}}
+            <section>
+                <span class="overlay"> </span>
+                <div class="modal-box">
+                    <h2>Detail Info</h2>
+                    <div class="detail-info">
+                        <div class="field">
+                            <div class="field-left">
+                                <div class="form-group-detail">
+                                    <label class="form-label" for="name">Nama</label>
+                                    <span id="name" class="form-control-plaintext">Darfito Danur</span>
+                                </div>
+                                <div class="form-group-detail">
+                                    <label class="form-label">Email</label>
+                                    <span class="form-control-plaintext">Himsi@gmail.com</span>
+                                </div>
+                                <div class="form-group-detail">
+                                    <label class="form-label">Nomor Telepon</label>
+                                    <span class="form-control-plaintext">+62*****</span>
+                                </div>
+                            </div>
+                            <div class="field-right">
+                                <div class="form-group-detail">
+                                    <label class="form-label">Jabatan</label>
+                                    <span class="form-control-plaintext">duduk aja</span>
+                                </div>
+                                <div class="form-group-detail">
+                                    <label class="form-label">Profil Linkedin</label>
+                                    <span class="form-control-plaintext">ini linknya gan</span>
+                                </div>
+                                <div class="form-group-detail">
+                                    <label class="form-label">Nama dan Alamat Perusahaan</label>
+                                    <span class="form-control-plaintext">krusty crab</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="buttons">
+                        <button type="button" class="close-btn">Close</button>
+                    </div>
+                </div>
+            </section>
+
         </main>
     </div>
 @endsection
 
 @section('extrajs')
-    {{-- <script src="{{ url('assets/js/landing.js') }}"></script> --}}
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const section = document.querySelector("section");
+            const overlay = document.querySelector(".overlay");
+            const showBtns = document.querySelectorAll(".show-info");
+            const closeBtn = document.querySelector(".close-btn");
+
+            showBtns.forEach(function(showBtn) {
+                showBtn.addEventListener("click", function(event) {
+                    event.preventDefault();
+                    section.classList.add("active");
+                });
+            });
+
+            overlay.addEventListener("click", function(event) {
+                event.preventDefault();
+                section.classList.remove("active");
+            });
+
+            closeBtn.addEventListener("click", function(event) {
+                event.preventDefault();
+                section.classList.remove("active");
+            });
+        });
+    </script>
 @endsection
---
+++ resources/views/koneksi/admin/alumni.blade.php
@@ -0,0 +1,163 @@
+@extends('template.koneksi.template-admin')
+
+@section('title')
+    Halo, {{ Auth::User()->name }}
+@endsection
+@section('seo-desc', ''){{-- ini buat deskripsi seo --}}
+@section('seo-img', '') {{-- ini buat gambar seo --}}
+
+@section('bodyclass', '') {{-- kalo butuh class buat body --}}
+@section('container', 'adm-alumniadd')
+
+
+@section('content')
+
+    <main class="main">
+        <div class="list">
+            {{-- Search bar --}}
+            <form class="search" autocomplete="off">
+                <div class="headline">
+                    <div class="headline__wrapper">
+                        <h4 class="headline__title">Data Alumni</h4>
+                        <label for="tahun">
+                            <select name="tahun" id="" class="headline__dropdown">
+                                <option value="">2019</option>
+                                <option value="">2018</option>
+                                <option value="">2017</option>
+                            </select>
+                        </label>
+                    </div>
+                    <a href="{{ url('/admin/alumni/add') }}" class="headline__add">
+                        <img src="{{ url('assets/img/alumni-add.svg') }}" alt="">
+                        <span>Baru</span>
+                    </a>
+                </div>
+                <div class="search__bar">
+                    <img src="{{ url('assets/img/search-alumni.svg') }}" alt="">
+                    <input type="search" class="search__bar__input" placeholder="Telusuri Data Alumni">
+                </div>
+            </form>
+            {{-- Main Content -> List of Alumni --}}
+            <div class="table">
+                <table class="table__content">
+                    <tr>
+                        <th>Nama</th>
+                        <th>Profil</th>
+                        <th>Email</th>
+                        <th>Bidang Pekerjaan</th>
+                        <th>Action</th>
+                    </tr>
+                    <tr>
+                        <td>Darfito Danur</td>
+                        <td><a href="">linkedin</a></td>
+                        <td>darfito@gmail.com</td>
+                        <td>Software Developer</td>
+                        <td>
+                            <div class="icons">
+                                <button class="show-info" type="button"><img
+                                        src="{{ url('assets/img/alumni-info.svg') }}"></button>
+                                <button type="button"><img src="{{ url('assets/img/alumni-edit.svg') }}"></button>
+                                <button type="button"><img src="{{ url('assets/img/alumni-delete.svg') }}"></button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Cakra Kusuma Erlangga Ramdani</td>
+                        <td><a href="">linkedin</a></td>
+                        <td>cak@gmail.com</td>
+                        <td>Web Developer</td>
+                        <td>
+                            <div class="icons">
+                                <button class="show-info" type="button"><img
+                                        src="{{ url('assets/img/alumni-info.svg') }}"></button>
+                                <button type="button"><img src="{{ url('assets/img/alumni-edit.svg') }}"></button>
+                                <button type="button"><img src="{{ url('assets/img/alumni-delete.svg') }}"></button>
+                            </div>
+
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        {{-- Pagination --}}
+        <div class="pagination">
+            <button>&lt</button>
+            <button class="selected">1</button>
+            <button>2</button>
+            <button>&gt</button>
+        </div>
+
+        {{-- modal --}}
+        <section>
+            <span class="overlay"> </span>
+            <div class="modal-box">
+                <h2>Detail Info</h2>
+                <div class="detail-info">
+                    <div class="field">
+                        <div class="field-left">
+                            <div class="form-group-detail">
+                                <label class="form-label" for="name">Nama</label>
+                                <span id="name" class="form-control-plaintext">Darfito Danur</span>
+                            </div>
+                            <div class="form-group-detail">
+                                <label class="form-label">Email</label>
+                                <span class="form-control-plaintext">Himsi@gmail.com</span>
+                            </div>
+                            <div class="form-group-detail">
+                                <label class="form-label">Nomor Telepon</label>
+                                <span class="form-control-plaintext">+62*****</span>
+                            </div>
+                        </div>
+                        <div class="field-right">
+                            <div class="form-group-detail">
+                                <label class="form-label">Jabatan</label>
+                                <span class="form-control-plaintext">duduk aja</span>
+                            </div>
+                            <div class="form-group-detail">
+                                <label class="form-label">Profil Linkedin</label>
+                                <span class="form-control-plaintext">ini linknya gan</span>
+                            </div>
+                            <div class="form-group-detail">
+                                <label class="form-label">Nama dan Alamat Perusahaan</label>
+                                <span class="form-control-plaintext">krusty crab</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="buttons">
+                    <button type="button" class="close-btn">Close</button>
+                </div>
+            </div>
+        </section>
+    </main>
+
+@endsection
+
+@section('extrajs')
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const section = document.querySelector("section");
+            const overlay = document.querySelector(".overlay");
+            const showBtns = document.querySelectorAll(".show-info");
+            const closeBtn = document.querySelector(".close-btn");
+
+            showBtns.forEach(function(showBtn) {
+                showBtn.addEventListener("click", function(event) {
+                    event.preventDefault();
+                    section.classList.add("active");
+                });
+            });
+
+            overlay.addEventListener("click", function(event) {
+                event.preventDefault();
+                section.classList.remove("active");
+            });
+
+            closeBtn.addEventListener("click", function(event) {
+                event.preventDefault();
+                section.classList.remove("active");
+            });
+        });
+    </script>
+
+@endsection
