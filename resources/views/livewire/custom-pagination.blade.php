ef8c279fd0a79a671a9c47db945980e07100b5a3
Thaofoo <91123146+Thaofoo@users.noreply.github.com>
2023-08-05 11:44:09 +0700

    fix: page navigator bug

--- resources/views/livewire/custom-pagination.blade.php
+++ resources/views/livewire/custom-pagination.blade.php
@@ -1,17 +1,17 @@
 <div class="pagination">
     @if (!$paginator->onFirstPage())
-    <button wire:click="previousPage">&lt</button>
+    <button wire:key="page-previous" wire:click="gotoPage({{ $paginator->currentPage()-1 }})">&lt</button>
     @else
     <button style="pointer-events: none; color:rgb(155, 155, 155)">&lt</button>
     @endif
 
     @for ($i = 1; $i <= $paginator->lastPage(); $i++)
-    <button wire:click="gotoPage({{ $i }})" class="{{ ($i == $paginator->currentPage()) ? 'selected' : '' }}">{{ $i }}</button>
+    <button wire:key="page-{{ $i }}" wire:click="gotoPage({{ $i }})" class="{{ ($i == $paginator->currentPage()) ? 'selected' : '' }}">{{ $i }}</button>
     @endfor
 
 
     @if ($paginator->hasMorePages())
-    <button wire:click="nextPage">&gt</button>
+    <button wire:key="page-next" wire:click="gotoPage({{ $paginator->currentPage()+1 }})">&gt</button>
     @else
     <button style="pointer-events: none; color:rgb(155, 155, 155)">&gt</button>
     @endif
