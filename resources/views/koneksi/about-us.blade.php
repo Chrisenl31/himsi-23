8608525a429825b9933e8c7c4b30e4f1f5e15103
Chrisenl31 <christianmarcello31@gmail.com>
2024-06-05 19:35:44 +0700

    Update Logo

--- resources/views/koneksi/about-us.blade.php
+++ resources/views/koneksi/about-us.blade.php
@@ -9,11 +9,12 @@
 
 @section('content')
     <div class="about--header">
-        <img src="{{ url('assets\image\logo-mercusuar-notitle.svg') }}" alt="" class="about--header__ornament" style="transform: scale(0.8); margin-top:80px">
+        <img src="{{ url('assets\image\kalibraksi\logo-kalibraksi-notitle-1.jpg') }}" alt=""
+            class="about--header__ornament" style="transform: scale(0.8); margin-top:90px">
         <div class="about--header__content">
-            <span class="about--header__koneksi midnight-blue">MERCUSUAR</span>
-            <h4 class="about--header__spelling grey">/MER • CU • SU • AR/</h4>
-            <h3 class="about--header__relation purple">Lighthouse</h3>
+            <span class="about--header__koneksi midnight-blue">KALIBRAKSI</span>
+            <h4 class="about--header__spelling grey">/KA • LI • BRAK • SI/</h4>
+            <h3 class="about--header__relation grey">Kalibrasi, Kolaborasi, Kontribusi</h3>
         </div>
     </div>
 
@@ -24,10 +25,14 @@
             Airlangga untuk <span class="midnight-blue">berkontribusi dan mengembangkan keilmuan dan keprofesian.</span>
         </h4>
         <h4 class="grey">HIMSI UNAIR berdiri sejak <span class="midnight-blue">26 November 2015</span>, dan pada
-            tahun ketujuh ini menggunakan nama kabinet <span class="midnight-blue">Mercusuar</span>. Menara yang dibangun di pantai, pulau kecil
-            di tengah laut, daerah berbatu karang, dan  sebagainya, yang memancarkan sinar isyarat pada waktu malam hari untuk
-            membantu navigasi. Seperti arti namanya, Kabinet <span class= "midnight-blue">Mercusuar</span> ingin menjadi sebuah menara yang tinggi <span class = "gold">(Dikenal)</span> ,
-            dengan fondasi yang kokoh <span class = "gold">(Kekeluargaan)</span>, serta membawa cahaya yang terang <span class = "gold">(Berdampak)</span>.
+            tahun kedelapan ini menggunakan nama kabinet <span class="midnight-blue">Kalibraksi</span>. Menara yang dibangun di
+            pantai, pulau kecil di tengah laut, daerah berbatu karang, dan sebagainya, yang memancarkan sinar isyarat pada
+            waktu malam hari
+            untuk membantu navigasi. Seperti arti namanya, Kabinet <span class= "midnight-blue">Mercusuar</span> ingin
+            menjadi
+            sebuah menara yang tinggi <span class = "gold">(Dikenal)</span> , dengan fondasi yang kokoh <span
+                class = "gold">(Kekeluargaan)</span>, serta membawa cahaya yang terang <span
+                class = "gold">(Berdampak)</span>.
     </div>
 
     <div class="about--bidang">
@@ -38,49 +43,57 @@
         </div>
         <div class="about--bidang__cards">
             <div class="about--bidang__card">
-                <img src="{{ url('assets/img/mercusuar/BPH.svg') }}" alt="BPH" class="about--bidang__card-img" width="60px">
+                <img src="{{ url('assets/img/mercusuar/BPH.svg') }}" alt="BPH" class="about--bidang__card-img"
+                    width="60px">
                 <h3 class="midnight-blue">Badan Pengurus Harian</h3>
                 <p class="grey">Pusat koordinasi bidang yang ada di HIMSI, serta memastikan proker dan agenda
                     yang direncanakan berjalan dengan sesuai.</p>
             </div>
             <div class="about--bidang__card">
-                <img src="{{ url('assets/img/mercusuar/Akademik.svg') }}" alt="Akademik" class="about--bidang__card-img" width="60px">
+                <img src="{{ url('assets/img/mercusuar/Akademik.svg') }}" alt="Akademik" class="about--bidang__card-img"
+                    width="60px">
                 <h3 class="midnight-blue">Akademik</h3>
                 <p class="grey">Meningkatkan kualitas akademik dan pendampingan dalam pengembangan prestasi
                     mahasiswa S1 SI UNAIR.</p>
             </div>
             <div class="about--bidang__card">
-                <img src="{{ url('assets/img/mercusuar/Ristek.svg') }}" alt="Ristek" class="about--bidang__card-img" width="60px">
+                <img src="{{ url('assets/img/mercusuar/Ristek.svg') }}" alt="Ristek" class="about--bidang__card-img"
+                    width="60px">
                 <h3 class="midnight-blue">Riset dan Teknologi</h3>
                 <p class="grey">Membantu, memfasilitasi dan memotivasi warga S1 SI UNAIR terhadap hal yang
                     berkaitan dengan keprofesian dan teknologi.</p>
             </div>
             <div class="about--bidang__card">
-                <img src="{{ url('assets/img/mercusuar/Media.svg') }}" alt="Media" class="about--bidang__card-img" width="60px">
+                <img src="{{ url('assets/img/mercusuar/Media.svg') }}" alt="Media" class="about--bidang__card-img"
+                    width="60px">
                 <h3 class="midnight-blue">Media</h3>
                 <p class="grey">Membuat desain grafis, video serta pengelolaan media sosial sebagai sarana
                     informasi warga S1 Sistem Informasi UNAIR dan pihak eksternal.</p>
             </div>
             <div class="about--bidang__card">
-                <img src="{{ url('assets/img/mercusuar/Sera.svg') }}" alt="Sera" class="about--bidang__card-img" width="60px">
+                <img src="{{ url('assets/img/mercusuar/Sera.svg') }}" alt="Sera" class="about--bidang__card-img"
+                    width="60px">
                 <h3 class="midnight-blue">Seni dan Olahraga</h3>
                 <p class="grey">Mewadahi dan memfasilitasi minat dan bakat warga S1 SI UNAIR di bidang
                     non-akademik khususnya dalam hal seni dan olahraga.</p>
             </div>
             <div class="about--bidang__card">
-                <img src="{{ url('assets/img/mercusuar/Hublu.svg') }}" alt="Hublu" class="about--bidang__card-img" width="60px">
+                <img src="{{ url('assets/img/mercusuar/Hublu.svg') }}" alt="Hublu" class="about--bidang__card-img"
+                    width="60px">
                 <h3 class="midnight-blue">Hubungan Luar</h3>
                 <p class="grey">Ranah kerja eskternal yang bertanggung jawab sebagai penghubung HIMSI UNAIR dengan
                     external HIMSI UNAIR.</p>
             </div>
             <div class="about--bidang__card">
-                <img src="{{ url('assets/img/mercusuar/Kestari.svg') }}" alt="Kestari" class="about--bidang__card-img" width="60px">
+                <img src="{{ url('assets/img/mercusuar/Kestari.svg') }}" alt="Kestari" class="about--bidang__card-img"
+                    width="60px">
                 <h3 class="midnight-blue">Kewirausahaan dan Inventaris</h3>
                 <p class="grey">Bertanggung jawab atas pemasukan dana kewirausahaan serta mengatur barang dan
                     berkas kesekretariatan HIMSI UNAIR.</p>
             </div>
             <div class="about--bidang__card">
-                <img src="{{ url('assets/img/mercusuar/PSDM.svg') }}" alt="PSDM" class="about--bidang__card-img" width="60px">
+                <img src="{{ url('assets/img/mercusuar/PSDM.svg') }}" alt="PSDM" class="about--bidang__card-img"
+                    width="60px">
                 <h3 class="midnight-blue">Pengembangan Sumber Daya Mahasiswa</h3>
                 <p class="grey">Bertanggung jawab pada dalam hal koordinasi, optimalisasi dan pengawalan sumber
                     daya mahasiswa S1 SI UNAIR.</p>
--- resources/views/template/koneksi/navbar.blade.php
+++ resources/views/template/koneksi/navbar.blade.php
@@ -1,18 +1,27 @@
 <div class="nav">
+    {{-- Nav Mobile --}}
     <div class="nav__mobile">
         <div class="nav__mobile__left">
-            <img onclick="location.href = '{{ route('home') }}'" src="{{ url('assets/image/logo.svg') }}" alt="" style="transform:scale(1.5)">
-            <img onclick="location.href = '{{ route('about-us') }}'" src="{{ url('assets/image/logo-mercusuar-notitle.svg') }}" alt="">
+            <img onclick="location.href = '{{ route('home') }}'" src="{{ url('assets/image/logo.svg') }}" alt=""
+                style="transform:scale(1.5)">
+            <img onclick="location.href = '{{ route('about-us') }}'"
+                src="{{ url('assets/image//kalibraksi/logo-kalibraksi-notitle-1.jpg') }}" alt=""
+                style="transform:scale(2.2)">
         </div>
         <div class="nav__burger">
             <span class="line"></span>
             <span class="line"></span>
         </div>
     </div>
+
+    {{-- Nav Desktop --}}
+
     <div class="nav__group">
         <div class="nav__left">
-            <img onclick="location.href = '{{ route('home') }}'" src="{{ url('assets/image/logo.svg') }}" alt="" width="45px">
-            <img onclick="location.href = '{{ route('about-us') }}'" src="{{ url('assets/image/logo-mercusuar.svg') }}" alt="" width="170px">
+            <img onclick="location.href = '{{ route('home') }}'" src="{{ url('assets/image/logo.svg') }}" alt=""
+                width="45px">
+            <img onclick="location.href = '{{ route('about-us') }}'"
+                src="{{ url('assets/image//kalibraksi/logo-kalibraksi.png') }}" alt="" width="150px">
         </div>
         <div class="nav__menu">
             <a href="{{ route('about-us') }}" class="nav__item">About Us</a>
